3326dfef4bc20fab520141908042411243638497
anilaaug <anila.sayone@gmail.com>
2018-06-20 10:29:17 +0530

    Second commit

--- app/src/main/java/com/example/anila/alertdialog/MainActivity.java
+++ app/src/main/java/com/example/anila/alertdialog/MainActivity.java
@@ -5,12 +5,21 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     AlertDialog.Builder builder;
-
+    //private int[] country;
+    String[] country = {"India", "USA", "China", "Japan", "Other"};
+    String[] language={"C","C++","Java","Python"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,5 +42,36 @@ public class MainActivity extends AppCompatActivity {
         });
         builder.create();
         builder.show();
+        Spinner spinner = findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(this);
+        List<String> categories = new ArrayList<String>();
+        categories.add("Yellow");
+        categories.add("Red");
+        categories.add("greeen");
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
+
+
+        ArrayAdapter<String> data=new ArrayAdapter<String>(this,android.R.layout.select_dialog_item,language);
+        AutoCompleteTextView ac=findViewById(R.id.autoCompleteTextView);
+        ac.setThreshold(1);
+        ac.setAdapter(data);
+        //ac.setTextColor(android.R.color.holo_red_light);
+        }
+
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // On selecting a spinner item
+        String item = parent.getItemAtPosition(position).toString();
+
+        // Showing selected spinner item
+        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+    }
+
+    public void onNothingSelected(AdapterView<?> arg0) {
+        Toast.makeText(this, "Nothing selected", Toast.LENGTH_SHORT).show();
     }
 }
+
+
+
